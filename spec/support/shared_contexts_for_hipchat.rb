73eeed423458c3ba1c26ebf135481f9835e335dd
Jon Evans <joncodes+github@gmail.com>
2017-06-20 09:08:26 -0600

    Add some specs for room pagination

--- spec/support/shared_contexts_for_hipchat.rb
+++ spec/support/shared_contexts_for_hipchat.rb
@@ -40,6 +40,17 @@ shared_context "HipChatV1" do
                                             :headers => {})
   end
 
+  def mock_successful_second_thousand_rooms
+    stub_request(:get, "https://api.hipchat.com/v1/rooms/list").with(
+                             :query => {:auth_token => "blah", :'max-results' => 1000, :'start-index' => 1000},
+                             :body => "",
+                             :headers => {'Accept' => 'application/json',
+                                          'Content-Type' => 'application/x-www-form-urlencoded'}).to_return(
+                                            :status => 200,
+                                            :body => '{"rooms":[{"room_id": "Hipchat", "links": {"self": "https://api.hipchat.com/v2/room/12345"}}]}',
+                                            :headers => {})
+  end
+
   def mock_successful_history(options={})
     options = { :date => 'recent', :timezone => 'UTC', :format => 'JSON', :'max-results' => 100, :'start-index' => 0 }.merge(options)
     canned_response = File.new(HISTORY_JSON_PATH)
@@ -181,6 +192,17 @@ shared_context "HipChatV2" do
   def mock_successful_rooms
     stub_request(:get, "https://api.hipchat.com/v2/room").with(
                              :query => {:auth_token => "blah"},
+                             :body => "",
+                             :headers => {'Accept' => 'application/json',
+                                          'Content-Type' => 'application/json'}).to_return(
+                                            :status => 200,
+                                            :body => '{"items":[{"id": "Hipchat", "links": {"self": "https://api.hipchat.com/v2/room/12345"}}]}',
+                                            :headers => {})
+  end
+
+  def mock_successful_second_thousand_rooms
+    stub_request(:get, "https://api.hipchat.com/v2/room").with(
+                             :query => {:auth_token => "blah", :'max-results' => 1000, :'start-index' => 1000},
                              :body => "",
                              :headers => {'Accept' => 'application/json',
                                           'Content-Type' => 'application/json'}).to_return(
